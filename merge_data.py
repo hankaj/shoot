2b15c361aa0318ccdea29774169fbfd9a4221270
hankaj <hanna.jarlaczynska@gmail.com>
2021-08-05 10:59:10 +0200

    Handle 40-shots data

--- merge_data.py
+++ merge_data.py
@@ -3,17 +3,20 @@ import re
 from os import listdir
 
 
-def drop_title_columns(df):
-    df.drop([1, 4, 7], inplace=True)
+def drop_title_columns(df, n_shots):
+    if n_shots == 60:
+        df.drop([1, 4, 7], inplace=True)
+    else:
+        df.drop([1, 4], inplace=True)
 
 
 def reshape(df):
-    return pd.DataFrame(df.values.reshape((1, 140)))
+    return pd.DataFrame(df.values.reshape((1, -1)))
 
 
-def drop_dummy_cols(df):
+def drop_dummy_cols(df, n_shots):
     df.drop(columns=[i for i in range(2, 20)], inplace=True)
-    df.drop(columns=[i for i in range(20, 140, 2)], inplace=True)
+    df.drop(columns=[i for i in range(20, 2 * n_shots + 20, 2)], inplace=True)
 
 
 def get_points_and_tens(result):
@@ -29,8 +32,8 @@ def get_day_date_time(dt):
     return day, date, time
 
 
-def rename_columns(df):
-    df.columns = [i for i in range(1, 61)]
+def rename_columns(df, n_shots):
+    df.columns = [i for i in range(1, n_shots + 1)]
 
 
 def add_columns(df, date, day, time, n_points, inner_tens):
@@ -41,26 +44,34 @@ def add_columns(df, date, day, time, n_points, inner_tens):
     df["inner_tens"] = inner_tens
 
 
-def refactor_data(data_dir):
-    df = pd.read_csv(f"csv_sius_data/60/{data_dir}", names=list(range(20)))
-    drop_title_columns(df)
+def refactor_data(data_dir, n_shots):
+    df = pd.read_csv(f"csv_sius_data/{n_shots}/{data_dir}", names=list(range(20)))
+    drop_title_columns(df, n_shots)
     df = reshape(df)
-    drop_dummy_cols(df)
+    drop_dummy_cols(df, n_shots)
     n_points, inner_tens = get_points_and_tens(df.loc[0, 0])
     day, date, time = get_day_date_time(df.loc[0, 1].split(" "))
     df.drop(columns=[0, 1], inplace=True)
-    rename_columns(df)
+    rename_columns(df, n_shots)
     add_columns(df, date, day, time, n_points, inner_tens)
     return df
 
 
-def export_data():
-    data_dirs = listdir("csv_sius_data/60")
+def get_dfs_to_concat(data_dirs, n_shots):
     if ".DS_Store" in data_dirs:
         data_dirs.remove(".DS_Store")
     dfs_to_concat = []
     for data_dir in data_dirs:
-        dfs_to_concat.append(refactor_data(data_dir))
+        dfs_to_concat.append(refactor_data(data_dir, n_shots))
+    return dfs_to_concat
+
+
+def export_data():
+    data_dirs_60 = listdir("csv_sius_data/60")
+    data_dirs_40 = listdir("csv_sius_data/40")
+    dfs_to_concat = get_dfs_to_concat(data_dirs_60, 60) + get_dfs_to_concat(
+        data_dirs_40, 40
+    )
     df_final = pd.concat(dfs_to_concat)
     df_final.to_csv("export_dataframe.csv", index=False, header=True)
 
